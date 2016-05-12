593f93b7c4a63be0953965a2f9e8d10e8da28cd3
dave <djungst@gmail.com>
2016-05-12 13:50:06 -0600

    add header

--- calc.rb
+++ calc.rb
@@ -1,7 +1,12 @@
-#print a welcome message
 #get first number
 #get operator
 #validate correct operator
 #get second number
 #print output
 #prompt to continue or exit
+
+
+def print_header
+  puts "Welcome to Ruby Calculator"
+  puts "__________________________"
+end
