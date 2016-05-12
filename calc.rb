87a801cc1a61c445c3b77b9cc86da677c47f58f8
dave <djungst@gmail.com>
2016-05-12 14:11:41 -0600

    add all functionality

--- calc.rb
+++ calc.rb
@@ -1,12 +1,33 @@
-#get first number
-#get operator
-#validate correct operator
-#get second number
-#print output
-#prompt to continue or exit
+def run
+  print_header
+  first_num = get_num('first')
+  op = operator
+  second_num = get_num('second')
+  puts first_num.public_send(op, second_num)
+end
 
+def get_num(place)
+  puts "Enter #{place} number"
+  gets.to_f
+end
 
 def print_header
   puts "Welcome to Ruby Calculator"
   puts "__________________________"
+end
+
+def operator
+  valid = false
+  until valid
+    puts "Enter operator (+, -, /, *)"
+    op = gets.strip
+    valid = op =~ /\+|-|\*|\//
+  end
+  op
+end
+
+while true
+  run
+  puts "Type q to exit or any key to continue"
+  exit if gets.strip.downcase == 'q'
 end
